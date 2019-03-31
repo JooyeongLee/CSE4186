a82d430bd276c6df3c0afffb0114e392b31c8715
JooyeongLee <48232337+JooyeongLee@users.noreply.github.com>
2019-03-31 21:31:09 +0900

    Update temperature_sensor.ino

--- WEEK2/temperature_sensor.ino
+++ WEEK2/temperature_sensor.ino
@@ -1,5 +1,4 @@
-// include ThingSpeak and ESP8266 WiFi Libraries
-#include "ThingSpeak.h"
+//delete thingspeak inst., using only mysql
 
 #include <TimeLib.h>
 #include <ESP8266WiFi.h>
@@ -38,9 +37,6 @@ MySQL_Connection conn((Client *)&client);
 
 int sensorValue; // variable to save analog reading
 
-//channel number and API key of ThingSpeak websie to transmit web
-unsigned long myChannelNumber = 727943;
-const char * myWriteAPIKey = "5EFJ86HKBSOOGAK5";
 
 void setup() {
   Serial.begin(115200);
@@ -68,7 +64,7 @@ void saveTempData() {
             Serial.println(sensors.getTempCByIndex(0));
             Serial.print("Query\n");
             set=0;
-            /*
+           
             dtostrf(sensors.getTempCByIndex(0), 2, 2, temperature);
             sprintf(query, INSERT_DATA, temperature);
             cur_mem->execute(query);
@@ -76,53 +72,11 @@ void saveTempData() {
             delete cur_mem;
             Serial.println("Data stored!");
             delay(1000*60);
-            */
+           
  }
-
-void _ThingSpeak(){
-  byte i, present=0;
-  byte type_s;
-  byte data[12];
-  byte addr[8];
-  float celsius;
-  Serial.print("while ThingSpeak . . .\n");
-  if(!ds.search(addr)){
-    ds.reset_search();
-    delay(250*60);
-    return;
-    }
-    if(OneWire::crc8(addr, 7)!=addr[7]){
-      Serial.println("CRC is not valid!");
-      return ;
-      }
-      if(addr[0] !=0x28){
-        Serial.println("Not DS18B20! ");
-        return ;}
-      ds.reset();
-      ds.select(addr);
-      ds.write(0x44,1);
-      delay(750*60);
-
-      present=ds.reset();
-      ds.select(addr);
-      ds.write(0xBE);
-      for(i=0; i<9; i++) data[i]=ds.read();
-      
-
-      int16_t raw=(data[1]<<8) | data[0];
-      celsius=(float)raw/16.0;
-      Serial.print("  Temperature = ");
-      Serial.println(celsius);
-      
-      
-      ThingSpeak.writeField(myChannelNumber, 1, celsius, myWriteAPIKey);
-      set=1;
-      }
 
 void loop() {
   
-  while(1){
-    if(set==0) _ThingSpeak();
-    else saveTempData();
+  saveTempData();
   }
 }
